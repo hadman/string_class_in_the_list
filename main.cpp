8fd5b3021748cb5b98d7db86ebb52eeb41190c76
hadman <falaza.studio@gmail.com>
2016-03-17 21:54:23 +1000

    new add method

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 using namespace std;
-struct oneChar
+struct elem
 {
     char info;
-    struct oneChar* ptr;
+    struct elem* ptr;
 };
 class strClass
 {
 private:
-    oneChar* head;
+    elem* head;
 public:
     strClass() // Конструктор
     {
@@ -19,11 +19,53 @@ public:
     {
 
     }
+    void add(char a)
+    {
+        elem* curr = head; // запоминаем
+        elem* e = new elem; // выделяем память под структуру
+        e -> info = a;
+        if(curr == NULL) // если строка была пустой
+        {
+            head = e;
+            head -> ptr = NULL; // указатель на конец строки
+        } else
+        {
+            while(curr->ptr != NULL) // ищем последний элемент списка
+            {
+                curr = curr -> ptr;
+            }
+            curr -> ptr = e;
+            e->ptr = NULL; // теперь e - последний элемент списка
+        }
+    }
+    void printStr()
+    {
+        elem* curr = head; // запоминаем
+        if(curr == NULL) // если строка была пустой
+        {
+            cout << "string is EMPTY!" << endl;
+        } else
+        {
+            while(curr->ptr != NULL) // ищем последний элемент списка
+            {
+                cout << curr->info;
+                curr = curr -> ptr;
+            }
+            cout << curr->info;
+            cout << endl;
+        }
+    }
 };
 
 
 int main()
 {
-
+    strClass str;
+    str.printStr();
+    str.add('a');
+    str.add('b');
+    str.add('c');
+    str.add('d');
+    str.printStr();
     return 0;
 }
