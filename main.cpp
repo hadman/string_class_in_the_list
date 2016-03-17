9a2bca504293211ee67a2f7222fd9b5b720b9fe3
hadman <falaza.studio@gmail.com>
2016-03-17 20:59:38 +1000

    create class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 
 using namespace std;
+struct oneChar
+{
+    char info;
+    struct oneChar* ptr;
+};
+class strClass
+{
+private:
+    oneChar* head;
+public:
+    strClass() // Конструктор
+    {
+        head = NULL;
+    }
+    ~strClass() // Деструктор
+    {
 
-int main() {
-    cout << "Hello, World!" << endl;
+    }
+};
+
+
+int main()
+{
+
     return 0;
 }
