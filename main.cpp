be4a63950b559e11abd38089f7d72d8e4b143da7
hadman <falaza.studio@gmail.com>
2016-03-18 19:44:05 +1000

    operator + overloading

--- main.cpp
+++ main.cpp
@@ -10,57 +10,6 @@ class strClass
 {
 private:
     elem* head;
-public:
-    strClass() // Конструктор
-    {
-        head = NULL;
-    }
-    ~strClass() // Деструктор
-    {
-
-    }
-
-    strClass& operator=(strClass& another)
-    {
-        if(head == another.head) // самоприсваивание
-        {
-            cout << "tut" << endl;
-            return *this;
-        }
-
-        elem* head = new elem;
-        elem* curr = another.head;
-
-        while (curr != NULL)
-        {
-            this->add(curr->info);
-            curr = curr->ptr;
-        }
-        return *this; // возвращаем голову нового списка
-    }
-
-
-    strClass& operator+(char a)
-    {
-        elem* curr = head; // запоминаем
-        elem* e = new elem; // выделяем память под структуру
-        e -> info = a;
-        if(curr == NULL) // если строка была пустой
-        {
-            head = e;
-            head -> ptr = NULL; // указатель на конец строки
-        } else
-        {
-            while(curr->ptr != NULL) // ищем последний элемент списка
-            {
-                curr = curr -> ptr;
-            }
-            curr -> ptr = e;
-            e->ptr = NULL; // теперь e - последний элемент списка
-        }
-        return *this;
-    }
-
     void add(char a)
     {
         elem* curr = head; // запоминаем
@@ -80,6 +29,66 @@ public:
             e->ptr = NULL; // теперь e - последний элемент списка
         }
     }
+public:
+    strClass() // Конструктор
+    {
+        head = NULL;
+    }
+    ~strClass() // Деструктор
+    {
+
+    }
+
+    strClass& operator=(strClass& another) // присваивание объекта
+    {
+        if(head == another.head) // самоприсваивание
+        {
+            return *this;
+        }
+
+        elem* head = new elem;
+        elem* curr = another.head;
+
+        while (curr != NULL)
+        {
+            this->add(curr->info);
+            curr = curr->ptr;
+        }
+        return *this; // возвращаем голову нового списка
+    }
+
+
+    strClass& operator+(char a) // конкатенация символа
+    {
+        this->add(a);
+        return *this;
+    }
+
+    strClass&operator+(string str) // конкатенация строки
+    {
+        for (int i = 0; i < str.length(); ++i) {
+            this->add(str[i]);
+        }
+        return *this;
+    }
+
+    strClass&operator+(strClass& another) // конкатенация объекта
+    {
+        elem* curr = another.head;
+        if(curr == NULL) // если объект пустой
+        {
+            return *this;
+        }else // если объект не пустой
+        {
+            while (curr != NULL)
+            {
+                this->add(curr->info);
+                curr = curr->ptr;
+            }
+        }
+        return *this;
+    }
+
     void printStr()
     {
         //cout << "head = " << head << endl;
@@ -106,12 +115,21 @@ int main()
     strClass str;
     strClass str2;
 
-    str.add('a');
-    str.add('b');
-    str.add('c');
-    str.add('d');
+    str = str + 'a';
+    str = str + 'b';
+    str = str + 'c';
+    str = str + 'd';
+    str = str + "hello";
 
+
+    str2 = str2 + " World";
+    str = str + str2;
+
+    cout << "stringClass Str = ";
     str.printStr();
+
+    cout << "stringClass Str2 = ";
+    str2.printStr();
 
     cout << "**********" << endl;
     cout << "**********" << endl;
