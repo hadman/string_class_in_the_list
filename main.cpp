8a9c67438b43d109fee9cd1854c5d439d74cf753
hadman <falaza.studio@gmail.com>
2016-03-18 18:49:03 +1000

    operator overloading c++ =

--- main.cpp
+++ main.cpp
@@ -19,6 +19,48 @@ public:
     {
 
     }
+
+    strClass& operator=(strClass& another)
+    {
+        if(head == another.head) // самоприсваивание
+        {
+            cout << "tut" << endl;
+            return *this;
+        }
+
+        elem* head = new elem;
+        elem* curr = another.head;
+
+        while (curr != NULL)
+        {
+            this->add(curr->info);
+            curr = curr->ptr;
+        }
+        return *this; // возвращаем голову нового списка
+    }
+
+
+    strClass& operator+(char a)
+    {
+        elem* curr = head; // запоминаем
+        elem* e = new elem; // выделяем память под структуру
+        e -> info = a;
+        if(curr == NULL) // если строка была пустой
+        {
+            head = e;
+            head -> ptr = NULL; // указатель на конец строки
+        } else
+        {
+            while(curr->ptr != NULL) // ищем последний элемент списка
+            {
+                curr = curr -> ptr;
+            }
+            curr -> ptr = e;
+            e->ptr = NULL; // теперь e - последний элемент списка
+        }
+        return *this;
+    }
+
     void add(char a)
     {
         elem* curr = head; // запоминаем
@@ -40,6 +82,7 @@ public:
     }
     void printStr()
     {
+        //cout << "head = " << head << endl;
         elem* curr = head; // запоминаем
         if(curr == NULL) // если строка была пустой
         {
@@ -61,11 +104,16 @@ public:
 int main()
 {
     strClass str;
-    str.printStr();
+    strClass str2;
+
     str.add('a');
     str.add('b');
     str.add('c');
     str.add('d');
+
     str.printStr();
+
+    cout << "**********" << endl;
+    cout << "**********" << endl;
     return 0;
 }
